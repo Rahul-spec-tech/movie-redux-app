baf3c510c5821985b47f14f3b91fa7940683a8c6
Rahul Nagaraj <rahul.nagaraj@mallow-tech.com>
2024-10-03 02:21:39 +0530

    Integrated show as well as movie card
    - Enhanced the transition of movie card in home page
    - Implemented the shows as like movies with card and plot details.
    - Implemented the Removal of show or movie data in the state or switching to the other card

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react';
 import MovieListing from "../MovieListing/MovieListing.js";
 import { useDispatch } from 'react-redux';
-import { fetchMovies } from '../../features/movies/movieSlice.js';
+import { fetchMovies, fetchShows } from '../../features/movies/movieSlice.js';
 
 const Home = () => {
     const dispatch = useDispatch();
     useEffect(() =>{
         const movie="Avatar";
+        const show="Friends";
         dispatch(fetchMovies(movie));
+        dispatch(fetchShows(show));
     }, [dispatch]);
 
     return (
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -9,16 +9,36 @@ export const fetchMovies = createAsyncThunk('movies/fetchMovies',
     }
 );
 
+export const fetchShows = createAsyncThunk('movies/fetchShows',
+    async (show) => {
+        const response = await movieApi.get(`?apiKey=${APIKey}&s=${show}&type=series`);
+        return response.data;
+    }
+);
+
+export const fetchMovieOrShowDetail = createAsyncThunk('movies/fetchMovieOrShowDetail',
+    async (id) => {
+        const response = await movieApi.get(`?apiKey=${APIKey}&i=${id}&Plot=full`);
+        return response.data;
+    }
+);
+
 const initialState = {
     movies: {},
+    shows: {},
+    moviesOrShowsDetail: {},
     loading: false,
     error: null,
-}
+};
 
 const movieSlice = createSlice({
     name: "movies",
     initialState,
-    reducers: {},
+    reducers: {
+        removeSelectedMovieOrShow: (state) => {
+            state.selectedMovieOrShow ={};
+        }
+    },
     extraReducers: (builder) => {
         builder
         .addCase(fetchMovies.pending, (state) => {
@@ -32,9 +52,36 @@ const movieSlice = createSlice({
         .addCase(fetchMovies.rejected, (state, action) => {
             state.loading = false;
             state.error = action.error.message;
+        })
+        .addCase(fetchShows.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        })
+        .addCase(fetchShows.fulfilled, (state, action) => {
+            state.loading = false;
+            state.shows = action.payload;
+        })
+        .addCase(fetchShows.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.error.message;
+        })
+        .addCase(fetchMovieOrShowDetail.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        })
+        .addCase(fetchMovieOrShowDetail.fulfilled, (state, action) => {
+            state.loading = false;
+            state.moviesOrShowsDetail = action.payload;
+        })
+        .addCase(fetchMovieOrShowDetail.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.error.message;
         });
     },
 });
 
+export const { removeSelectedMovieOrShow} = movieSlice.actions;
 export const getAllMovies = (state) => state.movies.movies;
+export const getAllShows = (state) => state.movies.shows;
+export const getAllMoviesOrShowsDetails = (state) => state.movies.moviesOrShowsDetail;
 export default movieSlice.reducer;
